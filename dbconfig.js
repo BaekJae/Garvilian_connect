436d661740227ce76d4e7c7f4aea1131f710e865
BaekJae <kis89576@naver.com>
2021-06-17 03:16:27 +0900

    Delete dbconfig.js

--- dbconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports= {
-    user: process.env.NODE_ORACLEDB_USER || "system",
-    password: process.env.NODE_ORACLEDB_PASSWORD || "1234",
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/XE",
-    externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-};
